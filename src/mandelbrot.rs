10bf179835a62a59be792872964c947f1bebb2aa
Matthew Watson <explodymatt@gmail.com>
2015-05-20 06:50:03 +1000

    Another near-complete rewrite. Now with easy-to-vectorise code.

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,68 +1,104 @@
 // The Computer Language Benchmarks Game
 // http://benchmarksgame.alioth.debian.org/
 //
-// contributed by the Rust Project Developers
-// contributed by TeXitoi
 // contributed by Matt Watson
+// contributed by TeXitoi
+
 use std::io::Write;
-use std::io;
 use std::thread;
 const THREADS: usize = 8;
 const MAX_ITER: usize = 50;
-const DX: f64 = -1.5;
-const DY: f64 = -1.0;
-pub fn mbrotpt(x: f64, y: f64) -> usize {
-    let mut z = (0.0, 0.0);
-    for _ in 0..MAX_ITER {
-        z = (z.0 * z.0 - z.1 * z.1 + x,
-             2.0 * z.0 * z.1 + y);
-        if z.0 * z.0 + z.1 * z.1 >= 4.0 {
-            return 0;
-        }
-    }
-    return 1;
+const VLEN: usize = 8;
+const ZEROS: Vecf64 = [0.0; VLEN];
+
+pub type Vecf64 = [f64; VLEN];
+
+fn mul2 (x: Vecf64, y: Vecf64) -> Vecf64 {
+    let mut res = ZEROS;
+    for i in 0..VLEN {res[i] = x[i] * y[i];}
+    res
+}
+fn add2 (x: Vecf64, y: Vecf64) -> Vecf64 {
+    let mut res = ZEROS;
+    for i in 0..VLEN {res[i] = x[i] + y[i];}
+    res
+}
+fn sub2 (x: Vecf64, y: Vecf64) -> Vecf64 {
+    let mut res = ZEROS;
+    for i in 0..VLEN { res[i] = x[i] - y[i]; }
+    res
 }
 
-fn mbrot8(x: usize, y: usize, inv: f64) -> u8 {
-    let mut result = 0 as usize;
-    let mut i = 0;
-    while i < 8 {
-        result = result << 1;
-        result = result | mbrotpt((x + i) as f64 * inv + DX,
-                                       y as f64 * inv + DY);
-        i += 1;
+pub fn mbrot8(cr: Vecf64, ci: Vecf64) -> u8 {
+    let mut zr = cr;
+    let mut zi = ci;
+    let mut esc_bits = 0;
+    for _ in 0..MAX_ITER {
+        // Find Re(z)^2 and Im(z)^2
+        let rr  = mul2(zr,zr);
+        let ii  = mul2(zi,zi);
+        // Check if we escape
+        // May as well store this info in
+        // same byte as output
+        let mag = add2(rr, ii);
+        for i in 0..VLEN {
+            if mag[i] > 4.0 {esc_bits |= 128 >> i}
+        }
+        // If no more work, break early
+        if esc_bits == 0xff {break}
+        // Find Im(z^2)
+        let ir = mul2(zr, zi);
+        // Set Re(z^2)
+        zr = sub2(rr, ii);
+        // Set Im(z^2)
+        zi = add2(ir, ir);
+        // Add c
+        zr = add2(zr, cr);
+        zi = add2(zi, ci);
     }
-    result as u8
+    !esc_bits
 }
 
 fn main() {
+
     let size = std::env::args_os().nth(1)
         .and_then(|s| s.into_string().ok())
         .and_then(|n| n.parse().ok())
         .unwrap_or(200);
     let inv = 2.0 / size as f64;
-    println!("P4");
-    println!("{} {}",size, size);
-    let workers: Vec<usize> = (0..THREADS).collect();;
-    let handles: Vec<_> = workers.into_iter().map(|t| {
+    let mut xvals = vec![0.0; size];
+    let mut yvals = vec![0.0; size];
+    for i in 0..size {
+        xvals[i] = i as f64 * inv - 1.5;
+        yvals[i] = i as f64 * inv - 1.0;
+    }
+    let xloc = &xvals;
+    let yloc = &yvals;
+    
+    let handles: Vec<_> = (0..THREADS).map(|e| {
+        let xloc = xloc.to_vec();
+        let yloc = yloc.to_vec();
         thread::spawn(move || {
-            let mut rows = vec![vec![0 as u8; 8 * size / 64]; size / THREADS];
-            for z in 0..size / THREADS {
-                let mut row = vec![0; size / 8];
-                for x in 0..size / 8 {
-                    row[x] = mbrot8(x * 8,t * (size / THREADS) + z, inv);
+        let mut rows = vec![vec![0 as u8; size / 8]; size / THREADS];
+        for y in 0..size / THREADS {
+            for x in 0..size / 8 {
+                let mut cr = ZEROS;
+                let ci = [yloc[y + e * size / THREADS]; VLEN];
+                for i in 0..VLEN {
+                    cr[i] = xloc[8 * x + i];
                 }
-                rows[z] = row.to_vec();
+                rows[y][x] = mbrot8(cr, ci);
             }
-            rows
+        }
+        rows
         })
     }).collect();
 
-    for h in handles {
-        let rows = h.join().unwrap();
-        for i in 0..size / THREADS {
-            std::io::stdout().write(&rows[i]).ok().expect("Could not write to stdout");
-        }
+    println!("P4\n{} {}", size, size);
+    let stdout_unlocked = std::io::stdout();
+    let mut stdout = stdout_unlocked.lock();
+    for row in handles.into_iter().flat_map(|h| h.join().unwrap().into_iter()) {
+        stdout.write_all(&row).unwrap();
     }
-    io::stdout().flush().ok().expect("Could not flush stdout");
+    stdout.flush().unwrap();
 }
