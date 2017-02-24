bab5d98f8dd214e7f01e957356bd9d74c00919ed
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2017-02-24 23:19:46 +0000

    mandelbrot: rayon and improvements (#40)

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -3,15 +3,13 @@
 //
 // contributed by Matt Watson
 // contributed by TeXitoi
+// contributed by Cristi Cobzarenco (@cristicbz)
 
-extern crate futures;
-extern crate futures_cpupool;
+extern crate rayon;
 
 use std::io::Write;
 use std::ops::{Add, Mul, Sub};
-use std::sync::Arc;
-use futures::Future;
-use futures_cpupool::{CpuPool, CpuFuture};
+use rayon::prelude::*;
 
 const MAX_ITER: usize = 50;
 const VLEN: usize = 8;
@@ -50,49 +48,96 @@ impl Sub for Vecf64 {
     }
 }
 
-pub fn mbrot8(cr: Vecf64, ci: Vecf64) -> u8 {
-    let mut zr = ZEROS;
-    let mut zi = ZEROS;
-    let mut tr = ZEROS;
-    let mut ti = ZEROS;
-    for _ in 0..MAX_ITER / 5 {
-        for _ in 0..5 {
-            zi = (zr + zr) * zi + ci;
-            zr = tr - ti + cr;
-            tr = zr * zr;
-            ti = zi * zi;
-        }
-        if (tr + ti).0.iter().all(|&t| t > 4.) {
-            return 0;
+pub struct Mandelbrot8 {
+    zr: Vecf64,
+    zi: Vecf64,
+    tr: Vecf64,
+    ti: Vecf64,
+
+    cr: Vecf64,
+    ci: Vecf64,
+    ci2: Vecf64,
+}
+
+impl Mandelbrot8 {
+    pub fn new(ci: Vecf64) -> Self {
+        Mandelbrot8 {
+            zr: ZEROS,
+            zi: ZEROS,
+            tr: ZEROS,
+            ti: ZEROS,
+
+            cr: ZEROS,
+            ci: ci,
+            ci2: ci * ci,
         }
     }
-    (tr + ti).0.iter()
-        .enumerate()
-        .map(|(i, &t)| if t <= 4. { 0x80 >> i } else { 0 })
-        .fold(0, |accu, b| accu | b)
+
+    pub fn run(&mut self, cr: Vecf64, cr2: Vecf64) -> u8 {
+        self.zr = cr;
+        self.zi = self.ci;
+        self.tr = cr2;
+        self.ti = self.ci2;
+        self.cr = cr;
+
+        self.advance(4);
+        for _ in 0..MAX_ITER / 5 - 1 {
+            if self.all_diverged() {
+                return 0;
+            }
+            self.advance(5);
+        }
+        self.to_byte()
+    }
+
+    fn advance(&mut self, iterations: usize) {
+        for _ in 0..iterations {
+            self.zi = (self.zr + self.zr) * self.zi + self.ci;
+            self.zr = self.tr - self.ti + self.cr;
+            self.tr = self.zr * self.zr;
+            self.ti = self.zi * self.zi;
+        }
+    }
+
+    fn all_diverged(&self) -> bool {
+        (self.tr + self.ti).0.iter().all(|&t| t > 4.)
+    }
+
+    fn to_byte(&self) -> u8 {
+        (self.tr + self.ti)
+            .0
+            .iter()
+            .enumerate()
+            .map(|(i, &t)| if t <= 4. { 0x80 >> i } else { 0 })
+            .fold(0, |accu, b| accu | b)
+    }
 }
+
 
 fn main() {
     let size = std::env::args().nth(1).and_then(|n| n.parse().ok()).unwrap_or(200);
     let size = size / VLEN * VLEN;
     let inv = 2. / size as f64;
-    let mut xloc = vec![ZEROS; size / VLEN];
+    let mut xloc = vec![(ZEROS, ZEROS); size / VLEN];
     for i in 0..size {
-        xloc[i / VLEN].0[i % VLEN] = i as f64 * inv - 1.5;
+        let x = i as f64 * inv - 1.5;
+        (xloc[i / VLEN].0).0[i % VLEN] = x;
+        (xloc[i / VLEN].1).0[i % VLEN] = x * x;
     }
-    let xloc = Arc::new(xloc);
-    let pool = CpuPool::new_num_cpus();
 
-    let future_rows: Vec<CpuFuture<Vec<_>, ()>> = (0..size).map(|y| {
-        let xloc = xloc.clone();
-        let ci = Vecf64([y as f64 * inv - 1.; VLEN]);
-        pool.spawn_fn(move || Ok((0..size / VLEN).map(|x| mbrot8(xloc[x], ci)).collect()))
-    }).collect();
+    let mut output = vec![0u8; size * size / VLEN];
+    output.par_chunks_mut(size / VLEN)
+        .weight_max()
+        .enumerate()
+        .for_each(|(y, chunk)| {
+            let mut m = Mandelbrot8::new(Vecf64([y as f64 * inv - 1.; VLEN]));
+            for (&(x, x2), c) in xloc.iter().zip(chunk) {
+                *c = m.run(x, x2);
+            }
+        });
 
     println!("P4\n{} {}", size, size);
     let stdout_unlocked = std::io::stdout();
     let mut stdout = stdout_unlocked.lock();
-    for row in future_rows {
-        stdout.write_all(&row.wait().unwrap()).unwrap();
-    }
+    stdout.write_all(&output).unwrap();
 }
