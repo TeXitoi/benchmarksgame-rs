5e282e4727c7af415f4c50686a0643996c80182b
Guillaume Pinot <texitoi@texitoi.eu>
2017-03-02 22:47:10 +0100

    binary_trees: limit recurtion depth + formating

--- src/binary_trees.rs
+++ src/binary_trees.rs
@@ -3,6 +3,7 @@
 //
 // contributed by the Rust Project Developers
 // contributed by Matt Brubeck
+// contributed by Cristi Cobzarenco (@cristicbz)
 // contributed by TeXitoi
 
 extern crate typed_arena;
@@ -14,28 +15,24 @@ use rayon::prelude::*;
 struct Tree<'a> {
     l: Option<&'a Tree<'a>>,
     r: Option<&'a Tree<'a>>,
-    i: i32
+    i: i32,
 }
 
 fn item_check(t: Option<&Tree>) -> i32 {
     match t {
         None => 0,
+        Some(&Tree { l: None, r: None, i }) => i,
         Some(&Tree { l, r, i }) => i + item_check(l) - item_check(r)
     }
 }
 
-fn bottom_up_tree<'r>(arena: &'r Arena<Tree<'r>>, item: i32, depth: i32)
-                  -> Option<&'r Tree<'r>> {
-    if depth > 0 {
-        let t: &Tree<'r> = arena.alloc(Tree {
-            l: bottom_up_tree(arena, 2 * item - 1, depth - 1),
-            r: bottom_up_tree(arena, 2 * item, depth - 1),
-            i: item
-        });
-        Some(t)
+fn bottom_up_tree<'r>(arena: &'r Arena<Tree<'r>>, i: i32, depth: i32) -> Option<&'r Tree<'r>> {
+    let (l, r) = if depth > 0 {
+        (bottom_up_tree(arena, 2 * i - 1, depth - 1), bottom_up_tree(arena, 2 * i, depth - 1))
     } else {
-        None
-    }
+        (None, None)
+    };
+    Some(arena.alloc(Tree { l: l, r: r, i: i }))
 }
 
 fn inner(depth: i32, iterations: i32) -> String {
@@ -45,43 +42,35 @@ fn inner(depth: i32, iterations: i32) -> String {
         let b = bottom_up_tree(&arena, -i, depth);
         item_check(a) + item_check(b)
     }).sum();
-    format!("{}\t trees of depth {}\t check: {}",
-            iterations * 2, depth, chk)
+    format!("{}\t trees of depth {}\t check: {}", iterations * 2, depth, chk)
 }
 
 fn main() {
-    let n = std::env::args_os().nth(1)
-        .and_then(|s| s.into_string().ok())
+    let n = std::env::args().nth(1)
         .and_then(|n| n.parse().ok())
         .unwrap_or(10);
     let min_depth = 4;
-    let max_depth = if min_depth + 2 > n {min_depth + 2} else {n};
+    let max_depth = if min_depth + 2 > n { min_depth + 2 } else { n };
 
     {
         let arena = Arena::new();
         let depth = max_depth + 1;
         let tree = bottom_up_tree(&arena, 0, depth);
-
-        println!("stretch tree of depth {}\t check: {}",
-                 depth, item_check(tree));
+        println!("stretch tree of depth {}\t check: {}", depth, item_check(tree));
     }
 
     let long_lived_arena = Arena::new();
     let long_lived_tree = bottom_up_tree(&long_lived_arena, 0, max_depth);
 
-    let messages = (min_depth/2..max_depth/2 + 1)
-            .into_par_iter()
-            .weight_max()
-            .map(|half_depth| {
-                let depth = half_depth * 2;
-                let iterations = 1 << ((max_depth - depth + min_depth) as u32);
-                inner(depth, iterations)
-            }).collect::<Vec<_>>();
+    let messages = (min_depth/2..max_depth/2 + 1).into_par_iter().weight_max().map(|half_depth| {
+            let depth = half_depth * 2;
+            let iterations = 1 << ((max_depth - depth + min_depth) as u32);
+            inner(depth, iterations)
+        }).collect::<Vec<_>>();
 
-    for message in messages.into_iter() {
+    for message in messages {
         println!("{}", message);
     }
 
-    println!("long lived tree of depth {}\t check: {}",
-             max_depth, item_check(long_lived_tree));
+    println!("long lived tree of depth {}\t check: {}", max_depth, item_check(long_lived_tree));
 }
