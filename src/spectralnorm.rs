696de8ee1230ac33455cd39d85aef58a45a6d8c8
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2017-03-04 12:51:36 +0000

    spectralnorm: rayon and de-vectorize

--- src/spectralnorm.rs
+++ src/spectralnorm.rs
@@ -4,58 +4,15 @@
 // contributed by the Rust Project Developers
 // contributed by Matt Brubeck
 // contributed by TeXitoi
+// contributed by Cristi Cobzarenco (@cristicbz)
 
 #![allow(non_snake_case)]
 
-use std::thread;
-
-// As std::simd::f64x2 etc. are unstable, we provide a similar interface,
-// expecting llvm to autovectorize its usage.
-#[allow(non_camel_case_types)]
-#[derive(Copy, Clone)]
-struct usizex2(usize, usize);
-impl std::ops::Add for usizex2 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        usizex2(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-impl std::ops::Mul for usizex2 {
-    type Output = Self;
-    fn mul(self, rhs: Self) -> Self {
-        usizex2(self.0 * rhs.0, self.1 * rhs.1)
-    }
-}
-impl std::ops::Div for usizex2 {
-    type Output = Self;
-    fn div(self, rhs: Self) -> Self {
-        usizex2(self.0 / rhs.0, self.1 / rhs.1)
-    }
-}
-impl From<usizex2> for f64x2 {
-    fn from(i: usizex2) -> f64x2 {
-        f64x2(i.0 as f64, i.1 as f64)
-    }
-}
-
-#[allow(non_camel_case_types)]
-struct f64x2(f64, f64);
-impl std::ops::Add for f64x2 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        f64x2(self.0 + rhs.0, self.1 + rhs.1)
-    }
-}
-impl std::ops::Div for f64x2 {
-    type Output = Self;
-    fn div(self, rhs: Self) -> Self {
-        f64x2(self.0 / rhs.0, self.1 / rhs.1)
-    }
-}
+extern crate rayon;
+use rayon::prelude::*;
 
 fn main() {
-    let n = std::env::args_os().nth(1)
-        .and_then(|s| s.into_string().ok())
+    let n = std::env::args().nth(1)
         .and_then(|n| n.parse().ok())
         .unwrap_or(100);
     let answer = spectralnorm(n);
@@ -75,60 +32,21 @@ fn spectralnorm(n: usize) -> f64 {
 }
 
 fn mult_AtAv(v: &[f64], out: &mut [f64], tmp: &mut [f64]) {
-    mult_Av(v, tmp);
-    mult_Atv(tmp, out);
+    mult(v, tmp, A);
+    mult(tmp, out, |i, j| A(j, i));
 }
 
-fn mult_Av(v: &[f64], out: &mut [f64]) {
-    parallel(out, |start, out| mult(v, out, start, Ax2));
+fn mult<F>(v: &[f64], out: &mut [f64], a: F)
+           where F: Fn(usize, usize) -> f64 + Sync {
+    out.par_iter_mut().enumerate().for_each(|(i, slot)| {
+        *slot = v.iter().enumerate().map(|(j, x)| x / a(i, j)).sum();
+    });
 }
 
-fn mult_Atv(v: &[f64], out: &mut [f64]) {
-    parallel(out, |start, out| mult(v, out, start, |i, j| Ax2(j, i)));
-}
-
-fn mult<F>(v: &[f64], out: &mut [f64], start: usize, a: F)
-           where F: Fn(usizex2, usizex2) -> f64x2 {
-    for (i, slot) in out.iter_mut().enumerate().map(|(i, s)| (i + start, s)) {
-        let mut sum = f64x2(0.0, 0.0);
-        for (j, chunk) in v.chunks(2).enumerate().map(|(j, s)| (2 * j, s)) {
-            let top = f64x2(chunk[0], chunk[1]);
-            let bot = a(usizex2(i, i), usizex2(j, j+1));
-            sum = sum + top / bot;
-        }
-        let f64x2(a, b) = sum;
-        *slot = a + b;
-    }
-}
-
-fn Ax2(i: usizex2, j: usizex2) -> f64x2 {
-    ((i + j) * (i + j + usizex2(1, 1)) / usizex2(2, 2) + i + usizex2(1, 1)).into()
+fn A(i: usize, j: usize) -> f64 {
+   ((i + j) * (i + j + 1) / 2 + i + 1) as f64
 }
 
 fn dot(v: &[f64], u: &[f64]) -> f64 {
-    v.iter().zip(u.iter()).map(|(a, b)| *a * *b).fold(0., |acc, i| acc + i)
-}
-
-struct Racy<T>(T);
-unsafe impl<T: 'static> Send for Racy<T> {}
-
-// Executes a closure in parallel over the given mutable slice. The closure `f`
-// is run in parallel and yielded the starting index within `v` as well as a
-// sub-slice of `v`.
-fn parallel<'a, T, F>(v: &mut [T], ref f: F)
-    where T: 'static + Send + Sync,
-          F: Fn(usize, &mut [T]) + Sync {
-    let size = v.len() / 4 + 1;
-    let jhs = v.chunks_mut(size).enumerate().map(|(i, chunk)| {
-        // Need to convert `f` and `chunk` to something that can cross the task
-        // boundary.
-        let f = Racy(f as *const F as *const usize);
-        let raw = Racy((&mut chunk[0] as *mut T, chunk.len()));
-        thread::spawn(move|| {
-            let f = f.0 as *const F;
-            let raw = raw.0;
-            unsafe { (*f)(i * size, std::slice::from_raw_parts_mut(raw.0, raw.1)) }
-        })
-    }).collect::<Vec<_>>();
-    for jh in jhs { jh.join().unwrap(); }
+    u.iter().zip(v).map(|(x, y)| x * y).sum()
 }
