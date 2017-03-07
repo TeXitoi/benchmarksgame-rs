c0c0056e0942fe7d0b6131a2e885accf61b729ce
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2017-03-07 20:34:20 +0000

    fannkuch: comments and reflow to 80 chars

--- src/fannkuch_redux.rs
+++ src/fannkuch_redux.rs
@@ -10,15 +10,24 @@ extern crate rayon;
 use std::{cmp, mem};
 use rayon::prelude::*;
 
+// This value controls the preferred maximum number of  blocks the workload is
+// broken up into. The actual value may be one higher (if the number of
+// permutations doesn't divide exactly by this value) or might be set to 1 if
+// the number of permutations is lower than this value.
 const NUM_BLOCKS: u32 = 24;
 
 fn fannkuch(n: i32) -> (i32, i32) {
+    // Precompute a table a factorials to reuse all over the place.
     let mut factorials = [1; 16];
-    for i in 1 .. n as usize + 1 {
+    for i in 1..n as usize + 1 {
         factorials[i] = factorials[i - 1] * i as u32;
     }
-
     let perm_max = factorials[n as usize];
+
+    // Compute the number of blocks and their size. If n! is less than
+    // NUM_BLOCKS then use a single block (perform the work serially for small
+    // values of n). If n! doesn't divide exactly by NUM_BLOCKS, then add one
+    // extra block to compute the remainder.
     let (num_blocks, block_size) = if perm_max < NUM_BLOCKS {
         (1, perm_max)
     } else {
@@ -26,10 +35,14 @@ fn fannkuch(n: i32) -> (i32, i32) {
          perm_max / NUM_BLOCKS)
     };
 
-    (0..num_blocks).into_par_iter().map(|i_block| i_block * block_size).map(|initial| {
+    // Compute the `checksum` and `maxflips` for each block in parallel.
+    (0..num_blocks).into_par_iter().map(|i_block| {
+        let initial = i_block * block_size;
         let mut count = [0i32; 16];
         let mut temp = [0i32; 16];
         let mut current = [0i32; 16];
+
+        // Initialise `count` and the current permutation (`current`)
         for (i, value) in current.iter_mut().enumerate() {
             *value = i as i32;
         }
@@ -44,37 +57,64 @@ fn fannkuch(n: i32) -> (i32, i32) {
             temp.copy_from_slice(&current);
             let d = d as usize;
             for j in 0..i + 1 {
-                current[j] = if j + d <= i { temp[j + d] } else { temp[j + d - i - 1] };
+                current[j] = if j + d <= i {
+                    temp[j + d]
+                } else {
+                    temp[j + d - i - 1]
+                };
             }
         }
 
-        let last_permutation_in_block = cmp::min(initial + block_size, perm_max) - 1;
+        // Iterate over each permutation in the block.
+        let last_permutation_in_block = cmp::min(initial + block_size,
+                                                 perm_max) - 1;
         let mut permutation_index = initial;
         let (mut checksum, mut maxflips) = (0, 0);
         loop {
+            // If the first value in the current permutation is not 1 (0) then
+            // we will need to do at least one flip for `current`.
             if current[0] > 0 {
-                let mut flip_count = 1;
+                // Copy the current permutation to work on it.
                 temp.copy_from_slice(&current);
 
+                // Flip `temp` (the copy of the current permutation) until its
+                // first element is 1 (0).
+                let mut flip_count = 1;
                 let mut first_value = current[0] as usize;
                 while temp[first_value] != 0 {
-                    let new_first_value = mem::replace(&mut temp[first_value], first_value as i32);
+                    let new_first_value = mem::replace(&mut temp[first_value],
+                                                       first_value as i32);
+                    // If the first value is greater than 3 (2), then we are
+                    // flipping a series of four or more values so we will need
+                    // to flip additional elements in the middle of `temp`.
                     if first_value > 2 {
                         temp[1..first_value].reverse();
                     }
+
+                    // Update `first_value` to the value we saved earlier and
+                    // record a flip in `flip_count`.
                     first_value = new_first_value as usize;
                     flip_count += 1;
                 }
 
-                checksum += if permutation_index % 2 == 0 { flip_count } else { -flip_count };
+                // Update the `checksum` and `maxflips` of this block.
+                checksum += if permutation_index % 2 == 0 {
+                    flip_count
+                } else {
+                    -flip_count
+                };
                 maxflips = cmp::max(maxflips, flip_count);
             }
 
+            // If this was the last permutation in the block, we're done: return
+            // the `checksum` and `maxflips` values which get reduced across
+            // blocks in parallel by `rayon`.
             if permutation_index >= last_permutation_in_block {
-                break;
+                return (checksum, maxflips);
             }
             permutation_index += 1;
 
+            // Generate the next permutation.
             let mut first_value = current[1];
             current[1] = current[0];
             current[0] = first_value;
@@ -91,14 +131,12 @@ fn fannkuch(n: i32) -> (i32, i32) {
             }
             count[i] += 1;
         }
-
-        (checksum, maxflips)
-    }).reduce(|| (0, 0), |(cs1, mf1), (cs2, mf2)| (cs1 + cs2, cmp::max(mf1, mf2)))
+    }).reduce(|| (0, 0),
+              |(cs1, mf1), (cs2, mf2)| (cs1 + cs2, cmp::max(mf1, mf2)))
 }
 
 fn main() {
-    let n = std::env::args_os().nth(1)
-        .and_then(|s| s.into_string().ok())
+    let n = std::env::args().nth(1)
         .and_then(|n| n.parse().ok())
         .unwrap_or(7);
 
